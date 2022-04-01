f404245ff3959167d00c8795a56a4e7237ebffd4
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2022-04-01 20:12:35 +0530

    refactor: removed logs

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,11 +3,8 @@ const hamContent = document.querySelector(".ham-content")
 const hamList = document.querySelector("#ham-list")
 
 function handleHam(e) {
-    console.log("clicked")
-
     if (hamList.className === "ham-list"){
         hamList.className += " mobile-view"
-        console.log(hamList.className)
     } else {
         hamList.className = "ham-list"     
     }
